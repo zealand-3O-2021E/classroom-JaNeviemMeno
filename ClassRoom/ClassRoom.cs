74b1f802273ea5058e8f6529e9509273442dca22
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-09-02 11:05:59 +0200

    two tasks

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,23 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class ClassRoom
+    {
+        public string ClassName
+        { get; set; }
+        public DateTime SemesterStart
+        {
+            get;set;
+        }
+        public List<T> ClassList
+        {
+            get;set;
+        }
+    }
+}
---
+++ ClassRoom/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthDay;
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            _name = name;
+            _birthDay = birthDay;
+            _birthMonth = birthMonth;
+        }
+
+        public string Name
+        { get;
+            private set;
+        }
+
+        public int BirthMonth
+        {
+            get;
+            private set;
+        }
+
+        public int BirthDay
+        {
+            get;
+            private set;
+        }
+
+    }
+}
